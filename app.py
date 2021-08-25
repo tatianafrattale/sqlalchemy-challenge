6f6e9e33d1ebecc4f5c729bfc5251b9105b7f6ab
tatianafrattale <tatianafrattale@gmail.com>
2021-08-24 22:40:11 -0400

    precipitation route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Import dependencies
+from types import prepare_class
 import numpy as np
 import datetime as dt
 
@@ -20,8 +21,17 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save references to each table
-Measurement = Base.classes.measurement
-Station = Base.classes.station
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Find the most recent date in the data set.
+session = Session(engine)
+recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+
+# Calculate the date one year from the last date in data set.
+one_year_from_date = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+session.close()
 
 ############################################
 # Flask Set up
@@ -47,7 +57,28 @@ def home():
         f"/api/v1.0/<start>/<end><br/>"
      )
 
+# Precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Session link
+    session = Session(engine)
+
+    """Return a list of Precipitation data"""
+    # Query
+    results = session.query(measurement.date, measurement.prcp).filter(measurement.date>=one_year_from_date).all()
+
+    session.close()
+
+    # Create dictionary
+    precipitation = []
+    for result in results:
+        precip = {}
+        precip[result[0]] = result[1]
+        precipitation.append(precip)
+
+    return jsonify(precipitation)
 
 
+# Run the app
 if __name__ == "__main__":
     app.run(debug=True)
