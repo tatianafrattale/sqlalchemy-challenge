27d42e28e79c333edbc94c6c50256962ebcccf8e
tatianafrattale <tatianafrattale@gmail.com>
2021-08-24 21:58:12 -0400

    setting up app dependencies and engine connections

---
+++ app.py
@@ -0,0 +1,28 @@
+# Import dependencies
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+###########################################
+
+# Setting up data base
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+############################################
+# Flask Set up
+app = Flask(__name__)
